a75bcc238e080fd3b1e8ecc4b20fa63f344a6b4d
Zhi Wang <zhi.wang@parashift.com.au>
2017-05-01 15:40:01 +0930

    Switch previewer type to 'embedded' from 'desktop'

--- share/src/main/resources/META-INF/components/preview/OnlyOffice.js
+++ share/src/main/resources/META-INF/components/preview/OnlyOffice.js
@@ -75,7 +75,7 @@
             var docType = docName.substring(docName.lastIndexOf(".") + 1).trim().toLowerCase();
 
             var config = {
-                type: "desktop",
+                type: "embedded",
                 width: "100%",
                 height: "100%",
                 documentType: getDocumentType(docType),
